7aa3cec93611cbec75d2a11477e672dd15be7fcf
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-12-11 11:25:03 +0200

    add class

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    public function query() {
+            // Dats Source Name = DSN
+        $dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;charset=utf8mb4";
+        
+            // PDO - PHP Data Object
+        $pdo = new PDO($dsn);
+
+            // Sagatavo vaicājumu (statement) 
+        $statement = $pdo->prepare("SELECT * FROM posts"); // prepare ir metode (līdzīk funkcijai)
+            // Izpildīt statement
+        $statement->execute();
+
+        // Dabūt rezultātu
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC); // var arī ievietot 2 iekavās
+        return $data;
+    }
+}
+?>
