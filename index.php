66f406aab9259bfce22b69ec2da7ab360bbbde67
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-06 11:03:49 +0200

    serch

--- index.php
+++ index.php
@@ -5,11 +5,29 @@ require "Database.php";
 $config = require("config.php");
 
 $db = new DATABASE($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC); // var arī ievietot 2 iekavās;
+$posts = $db->query("SELECT * FROM post")->fetchAll(PDO::FETCH_ASSOC); // var arī ievietot 2 iekavās;
 // dd($posts); // Izpilda funkciju dd
+?>
 
-echo"<ul>";
-foreach ($posts as $g) {
-    echo "<li> $g[content] </li>";
-}
-echo"</ul>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saraksts</title>
+</head>
+<body>
+    <h1>Saraksts</h1>
+    <form> <!-- ir difult action="index.php" method"GET" -->
+        <input name="q" type="text">
+        <button>Meklēt</button> <!-- Tas pacs kas <input type="submit" value="Meklēt"> -->
+    </form>
+    <?php
+        echo"<ul>";
+        foreach ($posts as $g) {
+            echo "<li> $g[content] </li>";
+        }
+        echo"</ul>";
+    ?>
+</body>
+</html>
