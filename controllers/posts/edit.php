c9fe93714d15279650e39bfb4d507cc69b038b67
Lanheiv <markussmpp@gmail.com>
2025-02-14 11:06:15 +0200

    komits

--- controllers/posts/edit.php
+++ controllers/posts/edit.php
@@ -20,7 +20,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors = [];
 
     $params = ["id" => $_GET["id"], "content" => $_POST["content"], "category_id" => $_POST["categories"]];
-    $sql = "UPDATE post SET content = :content  WHERE id = :id"; // fix part
+    $sql = "UPDATE post SET content = :content, category_id = :category_id WHERE id = :id";
 
     if(!Validator::string($_POST["content"], max: 50)) {
         $errors["content"] = "Saturam jābūt ievadītam, bet ne garākam par 50 rakstzīmēm";
