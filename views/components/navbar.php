30e3ecd275eda0d4a4957e7c8bba36f81c093a25
Lanheiv <markussmpp@gmail.com>
2025-02-18 09:48:03 +0200

    navbar style

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,9 +1,45 @@
-<nav>
-    <ul class="navbar">
-        <li><a href="/">Sākums</a></li>
-        <li><a href="/categories">Kategorijas</a></li>
-        <li><a href="/categories/create">Izveidot kategoriju</a></li>
-        <li><a href="/list/creat">Izveidot</a></li>
-        <li><a href="/list">Saraksts</a></li>
-    </ul>
-</nav>
+<div class="navbar">
+    <nav>
+        <a id="logo" href="/">
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="44"  height="44"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-assembly"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.27 2.27 0 0 1 -2.184 0l-6.75 -4.27a2.23 2.23 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98z" /><path d="M15.5 9.422c.312 .18 .503 .515 .5 .876v3.277c0 .364 -.197 .7 -.515 .877l-3 1.922a1 1 0 0 1 -.97 0l-3 -1.922a1 1 0 0 1 -.515 -.876v-3.278c0 -.364 .197 -.7 .514 -.877l3 -1.79c.311 -.174 .69 -.174 1 0l3 1.79h-.014z" /></svg>
+        </a>
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="/">
+                    Sākums
+                </a>
+            </li>
+
+            <li class="nav-item dropdown-box">
+                <a class="dropdown-block" href="">
+                    Izveidot
+                </a>
+                
+                <ul class="dropdown-menu">
+                    <li class="dropdown-item">
+                        <a href="/list/creat">Raksts</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="/categories/create">Kategorija</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown-box">
+                <a class="dropdown-block" href="">
+                    Skatīt
+                </a>
+                
+                <ul class="dropdown-menu">
+                    <li class="dropdown-item">
+                        <a href="/list">Raksts</a>
+                    </li>
+                    <li class="dropdown-item">
+                        <a href="/categories">Kategorija</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
